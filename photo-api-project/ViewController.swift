6eafb737643dd5e216d3541c3be745d1166908e8
Andrew Cook <Andrewcooktraining@yahoo.com>
2016-11-17 12:28:52 -0600

    Initial Commit

---
+++ photo-api-project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  photo-api-project
+//
+//  Created by andrew cook on 11/17/16.
+//  Copyright © 2016 andrew cook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
